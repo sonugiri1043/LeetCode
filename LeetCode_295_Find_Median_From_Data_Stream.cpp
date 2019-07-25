30b11221cfa54871bbb6d5665e39b692781fd661
Sonu Giri <sonugiri1043@gmail.com>
2019-07-25 20:27:34 +0530

    Find median from stream data

--- LeetCode_295_Find_Median_From_Data_Stream.cpp
+++ LeetCode_295_Find_Median_From_Data_Stream.cpp
@@ -1,55 +1,23 @@
 class MedianFinder {
   priority_queue<int> maxHeap;
   priority_queue<int, vector<int>, greater<int>> minHeap;
-    
 public:
   MedianFinder() {}
     
   void addNum( int num ) {
-    if( maxHeap.size() == 0 ) {
-      maxHeap.push( num );
-      return;
-    }
-    if( minHeap.size() == 0 ) {
-      if( num > maxHeap.top() ) {
-	minHeap.push( num );
-      } else {
-	minHeap.push( maxHeap.top() );
-	maxHeap.pop();
-	maxHeap.push( num );
-      }
-      return;
-    }
-        
-    if( maxHeap.size() == minHeap.size() ) {
-      if( num > maxHeap.top() && num < minHeap.top() ) {
-	maxHeap.push( num );
-      } else if( num < maxHeap.top() ) {
-	maxHeap.push( num );
-      } else {
-	maxHeap.push( minHeap.top() );
-	minHeap.pop();
-	minHeap.push( num );
-      }
-    } else {
-      if( num > maxHeap.top() && num < minHeap.top() ) {
-	minHeap.push( num );
-      } else if( num < maxHeap.top() ) {
-	minHeap.push( maxHeap.top() );
-	maxHeap.pop();
-	maxHeap.push( num );
-      } else {
-	minHeap.push( num );
-      }
+    maxHeap.push( num );
+    minHeap.push( maxHeap.top() );
+    maxHeap.pop();
+    if( maxHeap.size() < minHeap.size() ){
+      maxHeap.push( minHeap.top() );
+      minHeap.pop();
     }
   }
     
   double findMedian() {
-    if( maxHeap.size() == 0 ) return 0;
-    if( minHeap.size() == 0 ) return maxHeap.top();
-        
-    if( maxHeap.size() == minHeap.size() ) 
-      return ( maxHeap.top() + minHeap.top() )/2.0;
-    return maxHeap.top();
+    if( maxHeap.size() > minHeap.size() ) 
+      return (double) maxHeap.top();
+    else 
+      return ( maxHeap.top() + minHeap.top() ) * 0.5;
   }
 };
