faed60ba5a7466d506447f63f2c72f830917496b
Sonu Giri <sonugiri1043@gmail.com>
2019-08-11 22:06:06 +0530

    Diameter of Binary Tree

--- LeetCode_543_Diameter_of_Binary_Tree.cpp
+++ LeetCode_543_Diameter_of_Binary_Tree.cpp
@@ -71,3 +71,20 @@ public:
     return diameterHelper( node, depth );
   }
 };
+
+
+class Solution {
+  int result = 0;
+  int dfs( TreeNode* root ) {
+    if( !root ) return 0;
+    int left = dfs( root->left );
+    int right = dfs( root->right );
+    result = max( result, left + right );
+    return 1 + max( left, right );
+  }
+public:
+  int diameterOfBinaryTree(TreeNode* root) {
+    dfs( root );
+    return result;
+  }
+};
