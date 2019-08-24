18897c69a85616453b6f5b75f3e0af4e0b4d5333
Sonu Giri <sonugiri1043@gmail.com>
2019-08-24 23:15:33 +0530

    572 Subtree of Another Tree

---
+++ LeetCode_572_Subtree_of_Another_Tree.cpp
@@ -0,0 +1,59 @@
+/*
+  572. Subtree of Another Tree
+
+  Given two non-empty binary trees s and t, check whether tree t has exactly
+  the same structure and node values with a subtree of s. A subtree of s is a
+  tree consists of a node in s and all of this node's descendants. The tree s
+  could also be considered as a subtree of itself.
+
+  Example 1:
+  Given tree s:
+     3
+    / \
+   4   5
+  / \
+ 1   2
+
+ Given tree t:
+   4 
+  / \
+ 1   2
+ Return true, because t has the same structure and node values with a subtree
+ of s.
+
+ Example 2:
+ Given tree s:
+     3
+    / \
+   4   5
+  / \
+ 1   2
+    /
+   0
+
+   Given tree t:
+   4
+  / \
+ 1   2
+ Return false.
+*/
+
+class Solution {
+  void preOrder( TreeNode *node, string &s ) {
+    if( !node ) {
+      s += "#";
+      return;
+    }
+    s += "(" + to_string( node->val ) + ")";
+    preOrder( node->left, s );
+    preOrder( node->right, s );
+  }
+    
+public:
+  bool isSubtree( TreeNode* s, TreeNode* t ) {
+    string pre_s = "", pre_t = "";
+    preOrder( s, pre_s );
+    preOrder( t, pre_t );
+    return pre_s.find( pre_t ) != string::npos;
+  }
+};
