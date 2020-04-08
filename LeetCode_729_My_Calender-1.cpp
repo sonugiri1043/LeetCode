81483716caab15c8338ce1278c247006c6a1beb8
Sonu Giri <sonugiri1043@gmail.com>
2020-04-09 00:06:22 +0530

    Conflicting appointments

--- LeetCode_729_My_Calender-1.cpp
+++ LeetCode_729_My_Calender-1.cpp
@@ -34,3 +34,15 @@ public:
     return true;
   }
 };
+
+
+class MyCalendar {
+  map<int, int> events;
+public:
+  bool book( int start, int end )  {
+    auto next = events.upper_bound (start );
+    if( next != events.end() && (*next).second < end ) return false;
+    events.insert( {end,start} );
+    return true;
+  }
+};
