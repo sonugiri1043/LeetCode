81696b4981c243722dc442ab84deb4d5bcc4b009
Sonu Giri <sonugiri1043@gmail.com>
2020-04-13 02:09:31 +0530

    First missing number

--- LeetCode_41_First_Missing_positive.cpp
+++ LeetCode_41_First_Missing_positive.cpp
@@ -38,3 +38,34 @@ public:
     return max+1;
   }
 };
+
+int firstMissingPositive( vector<int>& nums ) {
+  int n = nums.size();
+  // Assume range = 1..n
+  // 1. Mark numbers outside range with a special marker number ( n+1 ) 
+  // ( we can ignore those outside range because even if one number
+  // is outside range then we will have one +ve missing within range.
+  for( int i=0; i<n; i++ ) {
+    if( nums[i] <= 0 || nums[i] > n ) 
+      nums[i] = n + 1;
+  }
+  // note: all number in the array are now positive, and on the range 1..n+1
+        
+  // 2. mark each cell appearing in the array, by converting the index for that number to negative
+  for( int i=0; i< n; i++ ) {
+    int no = abs( nums[i] );
+    if( no == n+1 ) continue;
+    if( nums[ no - 1 ] > 0 ) { // prevent double -ve operations
+      nums[ no - 1 ] *= -1;
+    }
+  }
+        
+  // 3. find the first cell which isn't negative (doesn't appear in the array)
+  for( int i=0; i < n; i++ ) {
+    if( nums[i] > 0 ) return i+1;
+  }
+        
+  // 4. no positive numbers were found, which means the array contains all numbers 1...n
+  return n+1;
+}
+};
